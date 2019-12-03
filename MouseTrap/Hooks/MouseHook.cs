2aaa3dea9513ce5c405822d38922d9e04a279f15
Temetra <temetra.xy+git@gmail.com>
2019-12-03 15:39:28 +0000

    Minor MouseHook optimisation

--- MouseTrap/Hooks/MouseHook.cs
+++ MouseTrap/Hooks/MouseHook.cs
@@ -1,6 +1,7 @@
 ﻿using MouseTrap.Data;
 using MouseTrap.Interop;
 using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 
 namespace MouseTrap.Hooks
@@ -13,14 +14,16 @@ namespace MouseTrap.Hooks
 		private bool _isDisposed;
 		private readonly HookProc _mouseHookCallback;
 		private IntPtr _mouseHookPtr;
-		private Dimensions _boundaries;
+		private int _xMin;
+		private int _xMax;
+		private int _yMin;
+		private int _yMax;
 		private bool _isRestricted;
 
 		public MouseHook()
 		{
 			_mouseHookCallback = new HookProc(MouseHookCallbackFunction);
 			_mouseHookPtr = IntPtr.Zero;
-			_boundaries = new Dimensions();
 			_isDisposed = false;
 		}
 
@@ -34,13 +37,13 @@ namespace MouseTrap.Hooks
 				// Set hook, using field to avoid issues with GetLastWin32Error
 				// GetLastWin32Error called immediately after SetWindowsHookEx
 				_mouseHookPtr = NativeMethods.SetWindowsHookEx(HookType.WH_MOUSE_LL, _mouseHookCallback, IntPtr.Zero, 0);
-				//int errorCode = Marshal.GetLastWin32Error();
+				int errorCode = Marshal.GetLastWin32Error();
 
 				// TODO improve error handling
 				if (_mouseHookPtr == IntPtr.Zero)
 				{
-					//string errorMessage = new Win32Exception(errorCode).Message;
-					//Logging.Logger.Write("Hook", $"Mouse error {errorCode} {errorMessage}");
+					string errorMessage = new Win32Exception(errorCode).Message;
+					Logging.Logger.Write($"Error {errorCode} {errorMessage}");
 				}
 			}
 		}
@@ -53,27 +56,23 @@ namespace MouseTrap.Hooks
 
 		public void SetRegion(Dimensions region)
 		{
-			_boundaries = new Dimensions(region.Left, region.Top, region.Right, region.Bottom);
+			_xMin = (int)region.Left;
+			_xMax = (int)region.Right;
+			_yMin = (int)region.Top;
+			_yMax = (int)region.Bottom;
 		}
 
 		public void RestrictMouseToRegion()
 		{
-			_isRestricted = true;
+			// Restrict to region if boundary is valid
+			var boundaryIsValid = (_xMax - _xMin > 0) && (_yMax - _yMin > 0);
+			if (boundaryIsValid == false) Logging.Logger.Write($"Boundary is not valid - x {_xMin}:{_xMax} y {_yMin}:{_yMax}");
+			_isRestricted = boundaryIsValid;
 		}
 
 		public void UnrestrictMouse()
 		{
 			_isRestricted = false;
-		}
-
-		private bool BoundaryIsValid
-		{
-			get
-			{
-				var width = _boundaries.Right - _boundaries.Left;
-				var height = _boundaries.Bottom - _boundaries.Top;
-				return (width > 0) && (height > 0);
-			}
 		}
 
 		private IntPtr MouseHookCallbackFunction(int code, IntPtr wParam, IntPtr lParam)
@@ -82,22 +81,23 @@ namespace MouseTrap.Hooks
 			var isMouseMoveMsg = (wParam.ToInt32() == 0x0200); // #define WM_MOUSEMOVE 0x0200
 
 			// Check if message should be handled
-			if (_isRestricted && isMouseMoveMsg && code >= 0 && BoundaryIsValid)
+			if (_isRestricted && isMouseMoveMsg && code >= 0)
 			{
 				// Get pointer data
 				var mouseInfo = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
-				var point = mouseInfo.pt;
+				var posX = mouseInfo.pt.X;
+				var posY = mouseInfo.pt.Y;
 
 				// Limit X
-				if (point.X < _boundaries.Left) point.X = (int)_boundaries.Left;
-				else if (point.X > _boundaries.Right) point.X = (int)_boundaries.Right;
+				if (posX < _xMin) posX = _xMin;
+				else if (posX > _xMax) posX = _xMax;
 
 				// Limit Y
-				if (point.Y < _boundaries.Top) point.Y = (int)_boundaries.Top;
-				else if (point.Y > _boundaries.Bottom) point.Y = (int)_boundaries.Bottom;
+				if (posY < _yMin) posY = _yMin;
+				else if (posY > _yMax) posY = _yMax;
 
 				// Move cursor
-				NativeMethods.SetCursorPos(point.X, point.Y);
+				NativeMethods.SetCursorPos(posX, posY);
 
 				// Done
 				return new IntPtr(1);
