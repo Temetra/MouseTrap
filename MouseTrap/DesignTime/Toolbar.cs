aaed30ac980dbee26ba7323ab0b8540e6fbbe165
Temetra <temetra.xy+git@gmail.com>
2019-12-10 21:46:22 +0000

    Toolbar visual changes

--- MouseTrap/DesignTime/Toolbar.cs
+++ MouseTrap/DesignTime/Toolbar.cs
@@ -5,25 +5,30 @@ namespace MouseTrap.DesignTime
 {
 	public class Toolbar : ViewModels.AppToolbar
 	{
-		public Toolbar() : this(ViewType.WindowList)
+		public Toolbar() : this(ViewType.FindProgram)
 		{
 		}
 
 		public Toolbar(ViewType viewType)
 		{
 			CurrentView = viewType;
+			WindowLockEnabled = true;
+
 			ChooseWindowCommand = new RelayCommand(
 				exe => { },
 				can => (CurrentView == ViewType.FindProgram)
 			);
+
 			FindProgramCommand = new RelayCommand(
 				exe => { },
 				can => (CurrentView == ViewType.WindowList)
 			);
+
 			ToggleLockCommand = new RelayCommand(
 				exe => { },
 				can => true
 			);
+
 			RefreshListCommand = new RelayCommand(
 				exe => { },
 				can => true
